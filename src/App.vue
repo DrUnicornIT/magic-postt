0f5181ef7fd9d6ccba524887fb993118db06a403
Nguyen Cong <congn1@ghtk.co>
2024-05-20 22:52:33 +0700

    add fxz

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+    <SideBar :key="$route.fullPath"/>
+  
+  <router-view />
+</template>
+
+<script setup>
+import axios from "axios";
+axios.defaults.headers.common.authorization = localStorage.getItem("token");
+import SideBar from "@/components/SideBar.vue";
+
+</script>
+<style>
+#app {
+  font-family: 'Nunito Sans', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  display:flex;
+  flex-direction: row;
+  padding: 0;
+  margin: 0;
+  max-width: none;
+}
+
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+  font-family: 'Nunito Sans', sans-serif;
+}
+
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+body {
+  background-color: #FFE4B2;
+  width: 100%;
+}
+
+.main {
+  display: flex;
+  flex-direction: row;
+}
+
+.app {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  main {
+    flex: 1 1 0;
+    padding: 2rem;
+
+    @media (max-width: 1024px) {
+      padding-left: 6rem;
+    }
+
+  }
+
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,35 @@
+<template>
+  <p>More about us - Coming soon</p>
+</template>
+
+<script></script>
+
+<style scoped>
+.container {
+  margin-left: 20px;
+  margin-right: 20px;
+  margin-top: 2.5vh;
+  margin-bottom: 2.5vh;
+  overflow-y: scroll;
+  height: 95vh;
+  border-radius: 12px;
+  transition: all 0.3s ease;
+}
+
+::-webkit-scrollbar {
+  display: none;
+}
+.banner {
+  display: flex;
+  grid-gap: 20px;
+  flex-wrap: wrap;
+  margin-bottom: 20px;
+  transition: all 0.3s ease;
+}
+
+.card {
+  height: fit-content;
+  flex-grow: 1;
+  flex-basis: 300px;
+}
+</style>
